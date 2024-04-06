84fc21cfbb8c7438a69bcad3939228b4d0a18b0a
lattanziob <brunoj.lattanzio@gmail.com>
2024-04-06 17:05:07 -0300

    Service/Controller token + firstcall + main

--- backend/main.mjs
+++ backend/main.mjs
@@ -1,59 +1,15 @@
-import axios from 'axios';
+import { ProductController } from "./controller/product_controller.mjs"
+import { TokenService } from "./service/token_service.mjs"
 
-const body = {
-    'grant_type': 'authorization_code',
-    'client_id': '275195427390645',
-    'client_secret': 'cMmJPpuM8oCgEpOQFguKNqgbL6l9PqGi',
-    'code': 'TG-660efbd89c21a40001269961-165015102',
-    'redirect_uri': 'https://localhost/overmeli'
+const token_service = new TokenService();
+const product_controller = new ProductController(token_service);
+
+
+async function main(){
+    await token_service.update_token();
+    await product_controller.first_call();
 }
 
-let token = 'BearerAPP_USR-275195427390645-040415-43e65a3a3d9d9fc4acff3e1e97063055-165015102';
-
-async function updateToken(){
-    const url = 'https://api.mercadolibre.com/oauth/token';
-
-    try {
-        const response = await axios.post(url, body );
-
-        token = 'Bearer' + response.data.access_token;
-        console.log(token)
-        return 'completo';
-
-    } catch (error) {
-        // Manejar errores aquí
-        console.error('Error en la solicitud de eventos:', error);
-        throw error;
-    }
-
-}
+main()
 
 
-
-async function firstCall() {
-
-    const header = {'Authorization': token}
-
-    const url = 'https://api.mercadolibre.com/products/search'
-    const params = {
-        'status': 'active',
-        'site_id': 'MLA',
-        'q': 'Samsung 20 Galaxy S8 64 GB rosa',
-
-    };
-
-
-    try {
-        const response = await axios.get(url, {params, headers: header});
-        console.log(response.data);
-        return 'completo';
-
-    } catch (error) {
-        // Manejar errores aquí
-        console.error('Error en la solicitud de eventos:', error);
-        throw error;
-    }
-}
-
-
-firstCall()
---
+++ backend/service/token_service.mjs
@@ -0,0 +1,23 @@
+import { TokenController } from "../controller/token_controller.mjs";
+
+export class TokenService{
+
+    constructor(){
+        this._auth_token = 'BearerAPP_USR-275195427390645-040415-43e65a3a3d9d9fc4acff3e1e97063055-165015102';
+        this._token_controller = new TokenController();
+    }
+
+    async update_token(){
+        await this._token_controller.update_token(this);
+    }
+
+    get_token(){
+
+        return this._auth_token;
+    }
+
+    set_token(token){
+        this._auth_token = token;
+    }
+
+}
