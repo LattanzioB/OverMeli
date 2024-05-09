e79422017cba007aa008d2887fcd2c508192f5ca
lattanziob <brunoj.lattanzio@gmail.com>
2024-05-09 18:10:20 -0300

    swagger start up

--- backend/LoginAPI/index.js
+++ backend/LoginAPI/index.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const {mongoose} = require('mongoose')
-const cookieParser = require('cookie-parser')
+const {mongoose} = require('mongoose');
+const cookieParser = require('cookie-parser');
 //const {url} = require('./config')
-
-const app = express();
+const {specs} = require('./swagger_config');
+const swaggerUi = require("swagger-ui-express");
 
 const {
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_PORT,
-    DB_NAME,
-  } = process.env;
-  
-  const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
+const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
 //database connection
 mongoose.connect(url) //Web: process.env.MONGO_URL //docker: url
 .then(()=> console.log('Database Connected'))
 .catch((err) => console.log('Database not Connected', err))
 
+
+const app = express();
 //middleware
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
-
 app.use('/', require('./routes/authRoutes'))
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 const port = 8000;
 app.listen(port, ()=> console.log(`server is running on port ${port}`))
--- backend/LoginAPI/routes/authRoutes.js
+++ backend/LoginAPI/routes/authRoutes.js
@@ -2,6 +2,71 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const {test, registerUser, loginUser, getProfile} = require('../controller/auth_controller')
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - userName
+ *         - password
+ *       properties:
+ *         userName:
+ *           type: string
+ *           description: The user name
+ *         password:
+ *           type: string
+ *           description: The password
+ *       example:
+ *         userName: user1
+ *         password: a2c4e6
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The users managing API
+ * /register:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ * /login:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The loged User.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
 
 //middleware
 router.use(
